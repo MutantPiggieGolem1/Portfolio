8ef5629b587880588de86f3cbce80bb26f5ea575
Steve Scruff <mutantpiggiegolems@gmail.com>
2023-07-13 11:56:24 -0700

    switch back to collisions, lock cursor

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,23 @@
-import { Animation, AnimationGroup, Engine, HavokPlugin, MeshBuilder, PhysicsAggregate, PhysicsShapeType, Scene, SceneLoader, SineEase, UniversalCamera, Vector3 } from "@babylonjs/core";
-import HavokPhysics from "@babylonjs/havok";
+import { Animation, AnimationGroup, Engine, MeshBuilder, Scene, SceneLoader, SineEase, UniversalCamera, Vector3 } from "@babylonjs/core";
 import { SkyMaterial } from "@babylonjs/materials";
-// import { Inspector } from '@babylonjs/inspector';
-
-//@ts-ignore
-globalThis.HK = await HavokPhysics(); 
+import { Inspector } from '@babylonjs/inspector';
 
 export function buildScene(engine: Engine) {
     const scene = new Scene(engine)
-    const physicsPlugin = new HavokPlugin();
-    scene.enablePhysics(undefined, physicsPlugin)
-    // Inspector.Show(scene, {"enableClose": true})
+    scene.disablePhysicsEngine()
+    scene.gravity = new Vector3(0, 9.8/60, 0);
+    scene.collisionsEnabled = true
+    Inspector.Show(scene, {"enableClose": true})
 
-    SceneLoader.AppendAsync("", "bioworld.babylon", scene)
-    .then(() => {
-        // const dome = scene.getMeshByName("Dome")!
+    SceneLoader.AppendAsync("", "bioworld.babylon", scene).then(() => {
+        const dome = scene.getMeshByName("Dome")!
+        dome.checkCollisions = true
 
         const pillars = scene.getMeshByName("Pillars")!
-        new PhysicsAggregate(pillars, PhysicsShapeType.CYLINDER, {mass: 375000}, scene)
+        pillars.checkCollisions = true
     })
 
-    const camera = new UniversalCamera("camera", new Vector3(0, 40, 0), scene)
+    const camera = new UniversalCamera("camera", new Vector3(0, 5, 0), scene)
     camera.attachControl(engine.getRenderingCanvas(), true);
     camera.inputs.addMouse(true)
     camera.inputs.addDeviceOrientation()
@@ -32,12 +29,34 @@ export function buildScene(engine: Engine) {
     camera.keysRight   = [68]; // D
     camera.speed = 0.5
 
+    //Mouse Pointer Lock
+    let isLocked = false;
+    scene.onPointerDown = () => {
+        if (isLocked) return
+        let canvas = engine.getRenderingCanvas()!;
+        canvas.requestPointerLock = canvas.requestPointerLock || canvas.msRequestPointerLock || canvas.mozRequestPointerLock || canvas.webkitRequestPointerLock;
+        if (canvas.requestPointerLock) canvas.requestPointerLock();
+    }
+    function onPointerLockChange() {
+        //@ts-ignore
+        isLocked = document.mozPointerLockElement || document.webkitPointerLockElement || document.msPointerLockElement || document.pointerLockElement || null;
+    }
+    document.addEventListener("pointerlockchange", onPointerLockChange, false);
+    document.addEventListener("mspointerlockchange", onPointerLockChange, false);
+    document.addEventListener("mozpointerlockchange", onPointerLockChange, false);
+    document.addEventListener("webkitpointerlockchange", onPointerLockChange, false);
+
+    camera.ellipsoid = new Vector3(0.7, 2, 0.7);
+    camera.checkCollisions = true
+    camera.applyGravity = true
+    camera.needMoveForGravity = true
+
     const skybox = MeshBuilder.CreateBox("skyBox", { size: 1000.0, updatable: false }, scene);
     skybox.disableEdgesRendering()
     const skyboxMaterial = new SkyMaterial("sky", scene);
     skyboxMaterial.disableDepthWrite = true;
     skyboxMaterial.backFaceCulling = false;
-    skyboxMaterial.luminance = 0.4
+    skyboxMaterial.luminance = 0.9
     skyboxMaterial.rayleigh = 3
     skyboxMaterial.turbidity = 3
     skyboxMaterial.useSunPosition = false
@@ -47,7 +66,7 @@ export function buildScene(engine: Engine) {
     sunAziEase.setEasingMode(SineEase.EASINGMODE_EASEINOUT)
     sunAzi.setEasingFunction(sunAziEase)
     const sunIncl = new Animation("timeincl", "inclination", 1, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_CYCLE, true)
-    sunIncl.setKeys([{frame: 0, value: 0}, {frame: 30, value: 0.5}, {frame: 60, value: 0}, {frame: 90, value: 0}])
+    sunIncl.setKeys([{frame: 0, value: 0}, {frame: 30, value: 0.5}, {frame: 60, value: 0}, {frame: 90, value: -0.5}])
     const sunAnim = new AnimationGroup("sunAnim", scene)
     sunAnim.addTargetedAnimation(sunAzi, skyboxMaterial)
     sunAnim.addTargetedAnimation(sunIncl, skyboxMaterial)
@@ -55,7 +74,7 @@ export function buildScene(engine: Engine) {
     skybox.material = skyboxMaterial;
 
     const ground = MeshBuilder.CreateGround("ground", {width: 1000, height: 1000}, scene)
-    new PhysicsAggregate(ground, PhysicsShapeType.MESH, {mass: 0}, scene)
+    ground.checkCollisions = true
 
     engine.runRenderLoop(() => scene.render());
 }
